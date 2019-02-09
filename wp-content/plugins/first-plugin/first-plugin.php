f2c54225965a99bbb66de2716750500e0b9f2d71
diana <diana.sell@khk.ee>
2019-02-09 16:23:50 +0200

    modified plugin `First plugin` version 1.0 (after deactivation of `First plugin` version 1.0)

--- wp-content/plugins/first-plugin/first-plugin.php
+++ wp-content/plugins/first-plugin/first-plugin.php
@@ -8,7 +8,36 @@ Author URI: http://dianasell.ikt.khk.ee/wp-epood/
 Version: 1.0
 */
  
-function my_plugin_test() {
-	echo 'Plugin töötab!'; exit;
+add_action('admin_bar_menu', 'add_toolbar_items', 100);
+function add_toolbar_items($admin_bar){
+    $admin_bar->add_menu( array(
+        'id'    => 'my-item',
+        'title' => 'My Item',
+        'href'  => '#',
+        'meta'  => array(
+            'title' => __('My Item'),            
+        ),
+    ));
+    $admin_bar->add_menu( array(
+        'id'    => 'my-sub-item',
+        'parent' => 'my-item',
+        'title' => 'My Sub Menu Item',
+        'href'  => '#',
+        'meta'  => array(
+            'title' => __('My Sub Menu Item'),
+            'target' => '_blank',
+            'class' => 'my_menu_item_class'
+        ),
+    ));
+    $admin_bar->add_menu( array(
+        'id'    => 'my-second-sub-item',
+        'parent' => 'my-item',
+        'title' => 'My Second Sub Menu Item',
+        'href'  => '#',
+        'meta'  => array(
+            'title' => __('My Second Sub Menu Item'),
+            'target' => '_blank',
+            'class' => 'my_menu_item_class'
+        ),
+    ));
 }
-add_action('admin_head', 'my_plugin_test');
